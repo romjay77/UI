a290216c09d834266b66a501f733fcbdd6e42ac9
Raman Sabchuk <romjay77@gmail.com>
2020-01-08 22:16:58 +0300

    fixed spelling

--- dist/UI/polyfills-es2015.js
+++ dist/UI/polyfills-es2015.js
@@ -6817,8 +6817,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! c:\Users\romja\Documents\GitHub\Crowd\Crowdfunding\Crowdfunding.UI\UI\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! c:\Users\romja\Documents\GitHub\Crowd\Crowdfunding\Crowdfunding.UI\UI\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! C:\Users\romja\Documents\GitHub\Crowd\Crowdfunding\Crowdfunding.UI\UI\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\Users\romja\Documents\GitHub\Crowd\Crowdfunding\Crowdfunding.UI\UI\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
